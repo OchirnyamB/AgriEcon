124715cb68ff51e69cbff63acb39d55a0497c71f
OchirnyamB <ochirnyam@gmail.com>
2016-10-17 11:45:53 -0500

    Initial commit

---
+++ src/view/welcomePage.java
@@ -0,0 +1,23 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class welcomePage extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("welcomePage.fxml"));
+        primaryStage.setTitle("Welcome Page");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
